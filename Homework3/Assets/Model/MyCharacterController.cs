c3a61b7c74002bf7e1d44df3a8fe4406b1728ee1
Zhao <2504197515@qq.com>
2018-04-11 01:14:33 +0800

    作业

---
+++ Homework3/Assets/Model/MyCharacterController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace myGame{
+	public class MyCharacterController {
+		public readonly float speed = 10;
+		readonly GameObject character;
+		readonly Moveable moveableScript;
+		readonly ClickGUI ClickGUI;
+		readonly int characterType;	
+
+
+		bool _isOnBoat;
+		CoastController coastController;
+
+
+		public MyCharacterController(string which_character) {
+
+			if (which_character.Contains("priest")) {
+				character = Object.Instantiate (Resources.Load ("Perfabs/Priest", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+				characterType = 0;
+			} else {
+				character = Object.Instantiate (Resources.Load ("Perfabs/Devil", typeof(GameObject)), Vector3.zero, Quaternion.identity, null) as GameObject;
+				characterType = 1;
+			}
+			moveableScript = character.AddComponent (typeof(Moveable)) as Moveable;
+			ClickGUI = character.AddComponent (typeof(ClickGUI)) as ClickGUI;
+			ClickGUI.setController (this);
+
+		}
+
+		public void setName(string name) {
+			character.name = name;
+		}
+
+		public void setPosition(Vector3 pos) {
+			character.transform.position = pos;
+		}
+
+		public GameObject getCharacter()
+		{
+			return character;
+		}
+
+		public Vector3 getPosition()
+		{
+			return character.transform.position;
+		}
+
+		public void moveToPosition(Vector3 destination) {
+			moveableScript.setDestination(destination);
+		}
+
+		public int getType() {	
+			return characterType;
+		}
+
+		public string getName() {
+			return character.name;
+		}
+
+		public void getOnBoat(BoatController boatCtrl) {
+			coastController = null;
+			character.transform.parent = boatCtrl.getGameobj().transform;
+			_isOnBoat = true;
+		}
+
+		public void getOnCoast(CoastController coastCtrl) {
+			coastController = coastCtrl;
+			character.transform.parent = null;
+			_isOnBoat = false;
+		}
+
+		public bool isOnBoat() {
+			return _isOnBoat;
+		}
+
+		public CoastController getCoastController() {
+			return coastController;
+		}
+
+		public void reset() {
+			moveableScript.reset ();
+			coastController = (Direct.getInstance ().currentSceneController as gameController).fromCoast;
+			getOnCoast (coastController);
+			setPosition (coastController.getEmptyPosition ());
+			coastController.getOnCoast (this);
+		}
+	}
+
+}
---
+++ Homework3/Assets/View/UserGUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using myGame;
+
+public class UserGUI : MonoBehaviour {
+	private UserAction action;
+	public int status = 0;
+	GUIStyle style;
+	GUIStyle buttonStyle;
+	GUIStyle button_Style;
+	bool isShow;
+
+
+	void Start() {
+		action = Direct.getInstance ().currentSceneController as UserAction;
+
+		style = new GUIStyle();
+		style.fontSize = 40;
+		style.alignment = TextAnchor.MiddleCenter;
+
+		buttonStyle = new GUIStyle("button");
+		buttonStyle.fontSize = 30;
+		button_Style=new GUIStyle("button");
+		button_Style.fontSize = 10;
+	}
+	void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 80, 30), "Rule", buttonStyle))
+		{
+			if (isShow)
+				isShow = false;
+			else
+				isShow = true;
+		}
+		if (isShow) {
+			GUI.Label (new Rect (Screen.width / 2 - 120, 10, 300, 50), "全部牧师和恶魔都渡河后即可获胜");
+			GUI.Label (new Rect (Screen.width / 2 - 120, 30, 300, 50), "每一边恶魔数量都不能多于牧师数量");
+			GUI.Label (new Rect (Screen.width / 2 - 120, 50, 300, 50), "绿色是牧师，红色是恶魔");
+		}
+		if (status == 1) {
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "Gameover!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "Restart", button_Style)) {
+				status = 0;
+				action.restart ();
+			}
+		} else if(status == 2) {
+			GUI.Label(new Rect(Screen.width/2-50, Screen.height/2-85, 100, 50), "You win!", style);
+			if (GUI.Button(new Rect(Screen.width/2-70, Screen.height/2, 140, 70), "Restart", buttonStyle)) {
+				status = 0;
+				action.restart ();
+			}
+		}
+	}
+}
