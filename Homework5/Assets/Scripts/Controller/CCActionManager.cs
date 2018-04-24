844b586622a156e8650dc5ac2bd8ae6a704930d5
Zhao <2504197515@qq.com>
2018-04-24 23:32:31 +0800

    作业

---
+++ Homework5/Assets/Scripts/Controller/CCActionManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CCActionManager : MonoBehaviour, IActionController {
+	public int Round { get; set; }
+	public int UFONum { get; private set; }
+	private UFOModel ufoModel = new UFOModel();
+	private Vector3 target;
+
+	List<GameObject> inUseUFOs;
+
+	public void Reset(int round) {
+		Round = round;
+		UFONum = round;
+		ufoModel.Reset(round);
+	}
+
+	public int GetRound() {
+		return Round;
+	}
+
+	public int GetUFONum() {
+		return UFONum;
+	}
+
+	public void SendUFO(List<GameObject> usingUFOs) {
+		inUseUFOs = usingUFOs;
+		Reset(Round);
+		Color UFOColor;
+		for (int i = 0; i < usingUFOs.Count; i++) {
+			int Ran = Random.Range (0, 3);
+			if (Ran < 1)
+				UFOColor = Color.red;
+			else if (Ran < 2)
+				UFOColor = Color.blue;
+			else
+				UFOColor = Color.green;
+			usingUFOs [i].GetComponent<Renderer> ().material.color = UFOColor;
+			Vector3 startPos;
+			if (Round % 2 == 1) {
+				startPos = new Vector3 (5f, 3f, -15f);
+			} else {
+				startPos =new Vector3 (-5f, 3f, -15f);
+			}
+			usingUFOs [i].transform.position = startPos;
+			usingUFOs[i].transform.position = startPos;
+
+			FirstSceneControllerBase.GetFirstSceneControllerBase().SetSceneStatus(SceneStatus.Shooting);
+		}
+	}
+
+	public void DestroyUFO(GameObject UFO) {
+		UFO.GetComponent<Rigidbody>().Sleep();
+		UFO.GetComponent<Rigidbody>().useGravity = false;
+		UFO.transform.position = new Vector3(0f, -99f, 0f);
+	}
+
+	public void SceneUpdate() {
+		Round++;
+		Reset(Round);
+	}
+
+	private void Start() {
+		Round = 1;
+		target = new Vector3(-3f, 10f, -2f);
+		Reset(Round);
+	}
+
+	private void Update() {
+		if (inUseUFOs != null) {
+			for (int i = 0; i < inUseUFOs.Count; i++) {
+				//Debug.Log(inUseUFOs[i].transform.localScale.y);
+				if (inUseUFOs[i].transform.position == target) {
+					//Debug.Log(inUseUFOs[i].transform.position.y);
+					FirstSceneControllerBase.GetFirstSceneControllerBase().DestroyUFO(inUseUFOs[i]);
+				} else {
+					inUseUFOs[i].transform.position = Vector3.MoveTowards(inUseUFOs[i].transform.position, target, 5 * Time.deltaTime);
+				}
+			}
+			if (inUseUFOs.Count == 0) {
+				FirstSceneControllerBase.GetFirstSceneControllerBase().SetSceneStatus(SceneStatus.Waiting);
+			}
+		}
+	}
+}
---
+++ Homework5/Assets/Scripts/Controller/Interface.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStatus {
+    Play, end
+}
+
+public enum SceneStatus {
+    Waiting, Shooting
+}
+
+public interface IUserInterface {
+    void SendUFO();
+    void DestroyUFO(GameObject ufo);
+}
+
+public interface IScore {
+	void AddScore();
+    int GetScore();
+}
+
+public interface IQueryStatus {
+    GameStatus QueryGameStatus();
+    SceneStatus QuerySceneStatus();
+}
+
+public interface ISetStatus {
+    void SetGameStatus(GameStatus gameStatus);
+    void SetSceneStatus(SceneStatus scenceStatus);
+}
+
+public interface IActionController {
+	int GetRound();
+	int GetUFONum();
+	void SendUFO(List<GameObject> usingUFOs);
+	void DestroyUFO(GameObject UFO);
+	void SceneUpdate();
+}
